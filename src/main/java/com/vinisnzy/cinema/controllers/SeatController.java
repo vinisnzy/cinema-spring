4cd71ed79fe8151015b40505b182d3aae09ea2aa
Vinícius de Paula <manoseilacara123@gmail.com>
2025-07-07 18:27:26 -0300

    feat: create SeatService and SeatController

---
+++ src/main/java/com/vinisnzy/cinema/controllers/SeatController.java
@@ -0,0 +1,60 @@
+package com.vinisnzy.cinema.controllers;
+
+import com.vinisnzy.cinema.models.seat.SeatRequestDTO;
+import com.vinisnzy.cinema.models.seat.SeatResponseDTO;
+import com.vinisnzy.cinema.services.SeatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/seats")
+@RequiredArgsConstructor
+public class SeatController {
+
+    private final SeatService service;
+
+    @GetMapping("/session/{id}")
+    public ResponseEntity<List<SeatResponseDTO>> getAllSeatsBySessionId(@PathVariable UUID id) {
+        return ResponseEntity.ok(service.getAllSeatsBySessionId(id));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SeatResponseDTO> getSeatById(@PathVariable UUID id) {
+        return ResponseEntity.ok(service.getSeatById(id));
+    }
+
+    @GetMapping("reserve/{id}")
+    public ResponseEntity<List<SeatResponseDTO>> getSeatsByReserveId(@PathVariable UUID id) {
+        return ResponseEntity.ok(service.getSeatsByReserveId(id));
+    }
+
+    @GetMapping("/session/{id}/reserved")
+    public ResponseEntity<List<SeatResponseDTO>> getReservedSeatsBySessionId(@PathVariable UUID id) {
+        return ResponseEntity.ok(service.getReservedSeatsBySessionId(id));
+    }
+
+    @GetMapping("/session/{id}/available")
+    public ResponseEntity<List<SeatResponseDTO>> getAvailableSeatsBySessionId(@PathVariable UUID id) {
+        return ResponseEntity.ok(service.getAvailableSeatsBySessionId(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<SeatResponseDTO> createSeat(@RequestBody SeatRequestDTO data) {
+        return ResponseEntity.ok(service.createSeat(data));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<SeatResponseDTO> updateSeat(@PathVariable UUID id, @RequestBody SeatRequestDTO data) {
+        return ResponseEntity.ok(service.updateSeat(id, data));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteSeat(@PathVariable UUID id) {
+        service.deleteSeat(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/com/vinisnzy/cinema/services/SeatService.java
@@ -0,0 +1,93 @@
+package com.vinisnzy.cinema.services;
+
+import com.vinisnzy.cinema.mappers.SeatMapper;
+import com.vinisnzy.cinema.models.seat.Seat;
+import com.vinisnzy.cinema.models.seat.SeatRequestDTO;
+import com.vinisnzy.cinema.models.reserve.Reserve;
+import com.vinisnzy.cinema.models.seat.SeatResponseDTO;
+import com.vinisnzy.cinema.models.session.Session;
+import com.vinisnzy.cinema.repositories.SeatRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class SeatService {
+
+    private final SeatRepository repository;
+    private final SessionService sessionService;
+    private final SeatMapper seatMapper;
+
+    public List<SeatResponseDTO> getAllSeatsBySessionId(UUID sessionId) {
+        return repository.findAllBySessionId(sessionId).stream()
+                .map(seat -> seatMapper.toResponseDTO(seat, sessionService)).toList();
+    }
+
+    public SeatResponseDTO getSeatById(UUID id) {
+        Seat seat = getEntityById(id);
+        return seatMapper.toResponseDTO(seat, sessionService);
+    }
+
+    public List<SeatResponseDTO> getSeatsByReserveId(UUID reserveId) {
+        return repository.findAllByReserveId(reserveId).stream()
+                .map(seat -> seatMapper.toResponseDTO(seat, sessionService)).toList();
+    }
+
+    public List<SeatResponseDTO> getReservedSeatsBySessionId(UUID sessionId) {
+        List<Seat> reservedSeats = repository.findAllBySessionIdAndReserved(sessionId, true);
+        return reservedSeats.stream()
+                .map(seat -> seatMapper.toResponseDTO(seat, sessionService)).toList();
+    }
+
+    public List<SeatResponseDTO> getAvailableSeatsBySessionId(UUID sessionId) {
+        List<Seat> freeSeats = repository.findAllBySessionIdAndReserved(sessionId, false);
+        return freeSeats.stream()
+                .map(seat -> seatMapper.toResponseDTO(seat, sessionService)).toList();
+    }
+
+    public SeatResponseDTO createSeat(SeatRequestDTO data) {
+        Seat seat = seatMapper.toEntity(data, sessionService);
+        return seatMapper.toResponseDTO(repository.save(seat), sessionService);
+    }
+
+    public SeatResponseDTO updateSeat(UUID id, SeatRequestDTO data) {
+        Seat seat = getEntityById(id);
+
+        Session session = sessionService.getEntityById(data.sessionId());
+        seat.setCode(data.code());
+        seat.setSession(session);
+
+        return seatMapper.toResponseDTO(repository.save(seat), sessionService);
+    }
+
+    public Seat getEntityById(UUID id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Seat not found"));
+    }
+    public List<Seat> getSeatsByCodesAndSessionId(List<String> codes, UUID sessionId) {
+        return repository.findByCodeInAndSessionId(codes, sessionId);
+    }
+
+    public void deleteSeat(UUID id) {
+        repository.deleteById(id);
+    }
+
+    public void saveSeat(Seat seat) {
+        repository.save(seat);
+    }
+
+    @Transactional
+    public void releaseSeatsFromReserve(Reserve reserve) {
+        List<Seat> seats = repository.findAllByReserveId(reserve.getId());
+
+        for (Seat seat : seats) {
+            seat.setReserved(false);
+            seat.setReserve(null);
+            repository.saveAndFlush(seat);
+        }
+    }
+}
