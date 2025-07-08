cd1f7c6480d32e3f0c35c56afd0c763f3868d7ea
Vinícius de Paula <manoseilacara123@gmail.com>
2025-07-07 21:30:15 -0300

    feat: add pagination and sort query to SeatRepository

--- src/main/java/com/vinisnzy/cinema/repositories/SeatRepository.java
+++ src/main/java/com/vinisnzy/cinema/repositories/SeatRepository.java
@@ -1,14 +1,53 @@
 package com.vinisnzy.cinema.repositories;
 
 import com.vinisnzy.cinema.models.Seat;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface SeatRepository extends JpaRepository<Seat, UUID> {
-    List<Seat> findAllBySessionId(UUID sessionId);
+    @Query(value = """
+        SELECT * FROM seat
+        WHERE session_id = :sessionId
+        ORDER BY
+          SUBSTRING(code FROM 1 FOR 1),
+          CAST(SUBSTRING(code FROM 2) AS INTEGER)
+        """,
+            countQuery = "SELECT COUNT(*) FROM seat WHERE session_id = :sessionId",
+            nativeQuery = true)
+    Page<Seat> findAllBySessionId(@Param("sessionId") UUID sessionId, Pageable pageable);
+
+    @Query(value = """
+        SELECT * FROM seat
+        WHERE reserve_id = :reserveId
+        ORDER BY
+          SUBSTRING(code FROM 1 FOR 1),
+          CAST(SUBSTRING(code FROM 2) AS INTEGER)
+        """,
+            countQuery = "SELECT COUNT(*) FROM seat WHERE reserve_id = :reserveId",
+            nativeQuery = true)
+    Page<Seat> findAllByReserveId(@Param("reserveId") UUID reserveId, Pageable pageable);
+
     List<Seat> findAllByReserveId(UUID reserveId);
-    List<Seat> findAllBySessionIdAndReserved(UUID sessionId, boolean reserved);
+
+    @Query(value = """
+        SELECT * FROM seat
+        WHERE session_id = :sessionId AND reserved = :reserved
+        ORDER BY
+          SUBSTRING(code FROM 1 FOR 1),
+          CAST(SUBSTRING(code FROM 2) AS INTEGER)
+        """,
+            countQuery = "SELECT COUNT(*) FROM seat WHERE session_id = :sessionId AND reserved = :reserved",
+            nativeQuery = true)
+    Page<Seat> findAllBySessionIdAndReserved(
+            @Param("sessionId") UUID sessionId,
+            @Param("reserved") boolean reserved,
+            Pageable pageable);
+
     List<Seat> findByCodeInAndSessionId(List<String> codes, UUID sessionId);
 }
