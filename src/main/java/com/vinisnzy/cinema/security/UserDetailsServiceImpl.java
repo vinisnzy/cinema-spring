f80b9e8209526efe8d27ec6b63f4c704d74402e9
Vinícius de Paula <manoseilacara123@gmail.com>
2025-07-11 20:19:27 -0300

    feat: create spring security configs and jwt token

---
+++ src/main/java/com/vinisnzy/cinema/security/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.vinisnzy.cinema.security;
+
+import com.vinisnzy.cinema.models.User;
+import com.vinisnzy.cinema.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = repository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                user.getAuthorities()
+        );
+    }
+}
